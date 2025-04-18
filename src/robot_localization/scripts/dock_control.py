e82f115132034bbc9051418521116cfcb1f3e639
leowang <leowang1996@gmail.com>
2025-04-18 20:44:21 +0800

    1

--- src/robot_localization/scripts/dock_control.py
+++ src/robot_localization/scripts/dock_control.py
@@ -453,8 +453,8 @@ class ArucoDockingController:
     def get_rot(self,pixel):
         u=int(pixel.x)
         v=int(pixel.y)
-        u_ax=np.arange(u-13,u+13)
-        v_ax=np.arange(v-13,v+13)
+        u_ax=np.arange(u-20,u+20)
+        v_ax=np.arange(v-20,v+20)
         if self.depth_image is not None:
             pt=[]
             for i in u_ax:
