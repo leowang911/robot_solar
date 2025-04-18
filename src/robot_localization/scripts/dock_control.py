e0234465e602b8b1cbec7bc3da65d97da11cde72
leowang <leowang1996@gmail.com>
2025-04-18 19:14:42 +0800

    1

--- src/robot_localization/scripts/dock_control.py
+++ src/robot_localization/scripts/dock_control.py
@@ -210,7 +210,7 @@ class ArucoDockingController:
         """清除过期数据"""
         current_time = rospy.Time.now()
         for marker_type in ['left', 'right', 'center', 'center_left', 'center_right']:
-            rospy.loginfo(f"{marker_type} current_time: {current_time} marker_time: {self.marker_time[marker_type]}")
+            # rospy.loginfo(f"{marker_type} current_time: {current_time} marker_time: {self.marker_time[marker_type]}")
                 
             if self.marker_time[marker_type] and \
                (current_time - self.marker_time[marker_type]).to_sec() > self.data_expiry:
@@ -294,8 +294,8 @@ class ArucoDockingController:
         # elif valid_right:
         #     self.state = "ESTIMATED_APPROACH"
         #     self.current_target = self.estimate_center('right')
-        for marker_type in ['left', 'right', 'center', 'center_left', 'center_right']:
-            rospy.loginfo(f"{marker_type} marker_time: {self.marker_time[marker_type]}")
+        # for marker_type in ['left', 'right', 'center', 'center_left', 'center_right']:
+        #     rospy.loginfo(f"{marker_type} marker_time: {self.marker_time[marker_type]}")
 
         if valid_center or valid_center_left or valid_center_right or valid_left or valid_right:
             self.state = "APPROACHING"
