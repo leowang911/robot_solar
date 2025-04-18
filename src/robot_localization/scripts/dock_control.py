85f9f5ed012e41300d2f0150f9e1ee2f661c5a5e
leowang <leowang1996@gmail.com>
2025-04-18 23:08:09 +0800

    1

--- src/robot_localization/scripts/dock_control.py
+++ src/robot_localization/scripts/dock_control.py
@@ -108,7 +108,7 @@ class ArucoDockingController:
         self.control_seq = 0
         
         #rospy.Timer(rospy.Duration(0.01), self.control_loop)
-        rospy.Timer(rospy.Duration(0.05), self.control_loop)
+        rospy.Timer(rospy.Duration(0.1), self.control_loop)
 
     def depth_cb(self, msg):
         data = np.frombuffer(msg.data, dtype=np.uint16 if msg.is_bigendian else '<u2')
