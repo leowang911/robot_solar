f72770bc28b2025ac050d315cbb35e74d6571304
leowang <leowang1996@gmail.com>
2025-04-18 10:29:45 +0800

    1

--- src/robot_localization/scripts/dock_control.py
+++ src/robot_localization/scripts/dock_control.py
@@ -758,9 +758,10 @@ class ArucoDockingController:
 
         self.control_pub.publish(control)
         if self.back==True:
-            self.back=False
             rospy.logwarn("start back move")
             time.sleep(1.0)
+            self.back=False
+
         self.control_seq += 1
 
 if __name__ == '__main__':
