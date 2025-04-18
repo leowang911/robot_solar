4dfea2a81926426acda62525d788cdd2dc1ec454
leowang <leowang1996@gmail.com>
2025-04-18 10:34:23 +0800

    1

--- src/robot_localization/scripts/dock_control.py
+++ src/robot_localization/scripts/dock_control.py
@@ -715,7 +715,7 @@ class ArucoDockingController:
                     if abs(self.get_marker_yaw(self.current_target['center'])) < 0.01:
                         self.back=True
                         control.distance = -200
-                        control.target_yaw = self.yaw_to_target_yaw_angle(self.get_marker_yaw(self.current_target['center']),self.current_yaw)
+                        control.target_yaw = self.yaw_to_target_yaw_angle(0,self.current_yaw)
                         control.robot_state = 2
                         rospy.logwarn("完成对正")
                         # control.robot_state = 1
