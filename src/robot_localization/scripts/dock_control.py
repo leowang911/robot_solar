b39efc79301e8102905fc9ce326104c6601e44d2
leowang <leowang1996@gmail.com>
2025-04-19 23:41:10 +0800

    1

--- src/robot_localization/scripts/dock_control.py
+++ src/robot_localization/scripts/dock_control.py
@@ -979,7 +979,7 @@ class ArucoDockingController:
                                 self.refine_align=False 
                                 return 
 
-                            if np.linalg.norm(target_vec) <0.10 and abs(target_vec[1])<0.02:
+                            if np.linalg.norm(target_vec) <0.15 and abs(target_vec[1])<0.02:
                                 control.robot_state = 4
                                 rospy.loginfo(f'GOOD *****************************************************start final docking')
 
