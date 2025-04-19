a21a30b81b9c506200e021cf65ea14d9a4052162
leowang <leowang1996@gmail.com>
2025-04-19 18:04:39 +0800

    1

--- src/robot_localization/scripts/dock_control.py
+++ src/robot_localization/scripts/dock_control.py
@@ -695,7 +695,7 @@ class ArucoDockingController:
             control.roller_speed = 0
             control.robot_state = 1
             self.control_pub.publish(control)
-            time.sleep(0.1)
+            time.sleep(0.5)
             control.robot_state = 2
             self.control_pub.publish(control)
             self.control_seq += 1
