72f3a0156caec0e6febad5787d35469a6facf9f0
leowang <leowang1996@gmail.com>
2025-05-08 22:29:54 +0800

    change first look

--- src/robot_localization/scripts/dock_control.py
+++ src/robot_localization/scripts/dock_control.py
@@ -84,7 +84,7 @@ class ArucoDockingController:
         self.control_seq = 0
         self.stop_flag = False
         self.out_dock_flag = False
-        self.in_dock_flag = False
+        self.in_dock_flag = True
         self.corner_finding_flag = True
         self.auto_cleaning_flag = True
         self.docking_flag = False
@@ -923,7 +923,7 @@ class ArucoDockingController:
                 # self.corner_finding_flag = False
                 # self.auto_cleaning_flag = False
 
-                if self.docking_flag ==False and False: #todo 
+                if self.docking_flag ==False: #todo 
                     if self.markers['left'] or self.markers['right'] or self.markers['center'] or self.markers['center_left'] or self.markers['center_right']:
                         self.state = "APPROACHING"
                         pass
