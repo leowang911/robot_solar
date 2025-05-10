b95b5743055345582fcb9eb296a595a90df2e287
leowang <leowang1996@gmail.com>
2025-05-10 19:44:38 +0800

    add newID command

--- src/robot_localization/scripts/dock_control.py
+++ src/robot_localization/scripts/dock_control.py
@@ -84,7 +84,7 @@ class ArucoDockingController:
         self.control_seq = 0
         self.stop_flag = False
         self.out_dock_flag = False
-        self.in_dock_flag = False
+        self.in_dock_flag = True
         self.corner_finding_flag = False
         self.auto_cleaning_flag = False
         self.docking_flag = False
