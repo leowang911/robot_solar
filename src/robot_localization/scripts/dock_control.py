37f959a401ceee42d6b047d4b635d4c2d4606506
leowang <leowang1996@gmail.com>
2025-05-15 11:50:02 +0800

    add fake gps

--- src/robot_localization/scripts/dock_control.py
+++ src/robot_localization/scripts/dock_control.py
@@ -36,9 +36,11 @@ class ArucoDockingController:
         self.target_yaw = 0# 目标航向角
         self.latitude = 30.32098151262
         self.longitude = 120.07004749195
-        self.latitude_drone = 30.32098151262
+        self.latitude_drone =30.32098566702 
+        # 30.32098151262
         # self.longitude_drone = -74.123339
-        self.longitude_drone = 120.07004749195
+        self.longitude_drone = 120.07102795217
+        # 120.07004749195
         self.gps_yaw = 0.0
         self.yaw_drone = 0.0
         self.speed = 0.0
@@ -217,8 +219,8 @@ class ArucoDockingController:
         self.current_yaw = math.radians(msg.yaw)
 
     def inspva_cb(self, msg):
-        # self.latitude = msg.latitude
-        # self.longitude = msg.longitude
+        self.latitude = msg.latitude
+        self.longitude = msg.longitude
         self.gps_yaw = math.radians(msg.yaw)
         
 
