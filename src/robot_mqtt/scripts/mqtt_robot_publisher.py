ed85bfc51b0957d5bfcfc0902f06a3c9afd95bcc
leowang <leowang1996@gmail.com>
2025-05-28 21:38:21 +0800

    1

--- src/robot_mqtt/scripts/mqtt_robot_publisher.py
+++ src/robot_mqtt/scripts/mqtt_robot_publisher.py
@@ -26,6 +26,7 @@ class MQTTRobotBridge:
         # self.mqtt_password = rospy.get_param('~mqtt_password', '123')
         self.robot_id = rospy.get_param('~robot_id', 'GFSTJM120250201')
         self.pub_topic = rospy.get_param('~pub_topic', f'robot/{self.robot_id}/status')
+        self.pub_topic2 = rospy.get_param('~pub_topic', f'robot/{self.robot_id}/debug')
         self.sub_topic = rospy.get_param('~sub_topic', 'robot/commands')
         self.uuid = str(uuid.uuid4())  # 生成唯一ID
         self.mqtt_connected = False
@@ -141,7 +142,7 @@ class MQTTRobotBridge:
         self.setup_mqtt()
 
     def control_callback(self, msg):
-        if msg.robot_state != 1:
+        if msg.robot_state != 8:
             self.robot_data["task_status"] = 0
         else:
             self.robot_data["task_status"] = 1
@@ -340,6 +341,8 @@ class MQTTRobotBridge:
             # 发布任务停止信号
             stop_msg = Bool(False)
             self.mission_pub.publish(stop_msg)
+        elif action == "change_state":
+            pass
             
         else:
             rospy.logwarn(f"Unknown mission action: {action}")
