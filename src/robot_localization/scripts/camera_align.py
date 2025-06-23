52fb616edea78ab1f7990631dbf14181bb897ea0
leowang911 <leowang1996@gmail.com>
2025-06-23 18:34:07 +0800

    add camera_align

--- src/robot_localization/scripts/camera_align.py
+++ src/robot_localization/scripts/camera_align.py
@@ -12,7 +12,7 @@ class WhiteLineDetector:
         
         # 参数设置
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber('/camera/image_raw', Image, self.image_callback)
+        self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.image_callback)
         self.angle_pub = rospy.Publisher('/selected_line_angle', Float32, queue_size=10)
         self.debug_pub = rospy.Publisher('/debug_image', Image, queue_size=1)  # 添加调试图像发布
         
